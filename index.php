78471315b913266bd89474cec9abd935b659382b
hevelmo <hevelmo060683@gmail.com>
2015-12-11 23:47:59 -0600

    commit

---
+++ index.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="es" class="no-js lt-ie10 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="es" class="no-js lt-ie10 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="es" class="no-js lt-ie10 lt-ie9"> <![endif]-->
+<!--[if IE 9]>         <html lang="es" class="no-js lt-ie10"> <![endif]-->
+<html class="no-js" lang="es">
+	<head>
+        <meta http-equiv="X-UA-Compatible" content="IE=EmulateIE7" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta http-equiv='cache-control' content='no-cache' />
+        <meta http-equiv='Content-Type' content='text/html; charset=UTF-8'>
+        <meta name='viewport' content='width=device-width, maximum-scale=1.0, minimum-scale=1.0, initial-scale=1.0' />
+
+		<meta class="temp" name="description" content="meta-content"/>
+		<meta class="temp" name="copyright"content="© Copyright year CORE">
+		<meta class="temp" name="robots" content="index, follow">
+		<link class="temp" rel="alternate" hreflang="es" href="http://url" />
+
+		<title>Core</title>
+
+        <link href="css/import.css" rel="stylesheet" type="text/css" />
+
+        <script>
+            var nav = navigator.appName;
+
+            if(nav == "Microsoft Internet Explorer"){
+                // Detectamos si nos visitan desde IE
+                if(nav == "Microsoft Internet Explorer"){
+                    // Convertimos en minusculas la cadena que devuelve userAgent
+                    var ie = navigator.userAgent.toLowerCase();
+                    // Extraemos de la cadena la version de IE
+                    var version = parseInt(ie.split('msie')[1]);
+
+                    // Dependiendo de la version mostramos un resultado
+                    switch(version){
+                        case 6:
+                            alert("Estas usando IE 6, es obsoleto. \n Para una visualización optima del sitio, te recomendamos utilizar \n lo más nuevo en navegadores Google Chrome, Mozilla FireFox, Internet Explorer a partir de las versiones v9, v10 y v11 ");
+                            break;
+                        case 7:
+                            alert("Estas usando IE 7, es obsoleto \n Para una visualización optima del sitio, te recomendamos utilizar \n lo más nuevo en navegadores Google Chrome, Mozilla FireFox, Internet Explorer a partir de las versiones v9, v10 y v11 ");
+                            break;
+                        case 8:
+                            alert("Estas usando IE 8, es obsoleto \n Para una visualización optima del sitio, te recomendamos utilizar \n lo más nuevo en navegadores Google Chrome, Mozilla FireFox, Internet Explorer a partir de las versiones v9, v10 y v11 ");
+                            break;
+                        /*case 9:
+                            alert("Para una visualización optima del sitio, te recomendamos utilizar \n lo más nuevo en navegadores Google Chrome, Mozilla FireFox, Internet Explorer a partir de las versiones v10 y v11 ");
+                            console.log("Estas usando IE 9, mas o menos compatible");
+                            break;*/
+                        default:
+                            console.log("Usas una version compatible");
+                            break;
+                    }
+                }
+            }
+        </script>
+	</head>
+	<body>        
+		<!-- Auxiliar Temporal Inputs's DIV -->
+    	<div id='hidden-inputs-session'></div>
+    	<!-- Auxiliar Temporal Inputs's DIV -->
+    	<div id='hidden-inputs-temporal'></div>
+    	<!--Templates's DIV-->
+    	<div class="wrapper_content_interactive" id='content-temporal-interactive'></div>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12">
+                    <nav id="colorNav">
+                        <ul>
+                            <li class="white marginbottom">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_ford_cavsa_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://tutorialzine.com/2012/10/css3-dropdown-menu/">Back to the tutorial</a></li>
+                                    <li><a href="http://tutorialzine.com/2012/10/css3-dropdown-menu/#comments">Get help</a></li>
+                                    <li><a href="http://tutorialzine.com/2012/10/css3-dropdown-menu/">Download this example</a></li>
+                                </ul>
+                            </li>
+                            <li class="white marginbottom">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_jaguar_land_rover_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="#">Payment</a></li>
+                                    <li><a href="#">Notifications</a></li>
+                                    <li><a href="#">Privacy options</a></li>
+                                    <li><a href="#">Account Settings</a></li>
+                                </ul>
+                            </li>
+                            <li class="white marginbottom">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_lincoln_cavsa_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://twitter.com/Tutorialzine">Follow us on Twitter</a></li>
+                                    <li><a href="http://www.facebook.com/Tutorialzine">Find us on Facebook</a></li>
+                                </ul>
+                            </li>
+                            <li class="white marginbottom">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_mazda_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://tutorialzine.com/tag/php/">PHP tutorials</a></li>
+                                    <li><a href="http://tutorialzine.com/tag/jquery/">jQuery techniques</a></li>
+                                    <li><a href="http://tutorialzine.com/tag/css/">CSS articles</a></li>
+                                    <li><a href="http://tutorialzine.com/category/tutorials/">Everything else</a></li>
+                                </ul>
+                            </li>
+                            <li class="white marginbottom">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_syc_motors_chrysler_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://tutorialzine.com/contact/">Contact us</a></li>
+                                </ul>
+                            </li>
+                            <li class="white marginbottom">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_mitsubishi_motors_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://tutorialzine.com/contact/">Contact us</a></li>
+                                </ul>
+                            </li>
+                            <li class="white margintop">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_mercedes_benz_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://tutorialzine.com/2012/10/css3-dropdown-menu/">Back to the tutorial</a></li>
+                                    <li><a href="http://tutorialzine.com/2012/10/css3-dropdown-menu/#comments">Get help</a></li>
+                                    <li><a href="http://tutorialzine.com/2012/10/css3-dropdown-menu/">Download this example</a></li>
+                                </ul>
+                            </li>
+                            <li class="white margintop">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_volvo_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="#">Payment</a></li>
+                                    <li><a href="#">Notifications</a></li>
+                                    <li><a href="#">Privacy options</a></li>
+                                    <li><a href="#">Account Settings</a></li>
+                                </ul>
+                            </li>
+                            <li class="white margintop">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_vw_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://twitter.com/Tutorialzine">Follow us on Twitter</a></li>
+                                    <li><a href="http://www.facebook.com/Tutorialzine">Find us on Facebook</a></li>
+                                </ul>
+                            </li>
+                            <li class="white margintop">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_suzuki_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://tutorialzine.com/tag/php/">PHP tutorials</a></li>
+                                    <li><a href="http://tutorialzine.com/tag/jquery/">jQuery techniques</a></li>
+                                    <li><a href="http://tutorialzine.com/tag/css/">CSS articles</a></li>
+                                    <li><a href="http://tutorialzine.com/category/tutorials/">Everything else</a></li>
+                                </ul>
+                            </li>
+                            <li class="white margintop">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_peugeot_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://tutorialzine.com/contact/">Contact us</a></li>
+                                </ul>
+                            </li>
+                            <li class="white margintop">
+                                <a href="#" class="">
+                                    <img src="img/agencias/brands/logo_ssandier_brp_140x82.png" alt="">
+                                </a>
+                                <ul>
+                                    <li><a href="http://tutorialzine.com/contact/">Contact us</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+
+
+		<!-- MAIN -->
+		<script src="lib/jquery.js"></script>
+		<script src="lib/modernizr.js"></script>
+		<script src="lib/bootstrap.js"></script>
+		<!-- CORE -->
+		<script src="lib/jquery.gdb.js"></script>
+		<script src="lib/jquery-ui.js"></script>
+		<script src="lib/underscore.js"></script>
+		<script src="lib/moment.js"></script>
+		<script src="lib/accounting.js"></script>
+		<script src="lib/finch.js"></script>
+		<!-- HANDLEBARS -->
+		<script src="lib/handlebars.runtime.js"></script>
+		<!-- TEMPLATES -->
+		<script src='templates/min/templates.min.js'></script>
+		<!-- FORMS -->
+		<script src="lib/forms.js"></script>
+		<script src="lib/sha512.js"></script>
+		<!-- PLUGINS -->
+		<!-- CORE JS -->
+        <?php /*
+		<script src='js/min/core.min.js'></script>
+        */ ?>
+	    <script src='js/objects.js'></script>
+	    <script src='js/method.js'></script>
+	    <script src='js/model.js'></script>
+	    <script src='js/room.js'></script>
+	    <script src='js/main.js'></script>
+	</body>
+</html>
+
